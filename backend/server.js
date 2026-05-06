e28e0381adf5e5a403a8ac293836cd10b3ecb4cc
Karthik <karthik@example.com>
2026-05-06 07:14:41 +0530

    prepare for vercel

--- backend/server.js
+++ backend/server.js
@@ -101,10 +101,13 @@ const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   })
   .catch((err) => {
     console.error('Failed to connect to MongoDB', err);
   });
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+
+module.exports = app;
